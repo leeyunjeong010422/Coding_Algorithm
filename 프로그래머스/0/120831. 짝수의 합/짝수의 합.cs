76a72413e0052c6187876d8fb4098f81531f3115
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-07-22 23:08:51 +0900

    [level 0] Title: 짝수의 합, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int sum = 0;
+        
+        for(int i = 0; i <= n; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        
+        return sum;
+    }
+}
